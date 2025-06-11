b0073329002739a969d446beac51a0d9c17dfbf9
Lydia Neema <lydianeema25@gmail.com>
2025-06-11 20:28:59 +0300

    updated the html using the dom

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let body=document.querySelector(".body");
+body.removeChild(main);
+
+let newHeader=document.createElement("h1");
+newHeader.id="victory";
+newHeader.innerHTML="Lydia is the champion";
+
+
+
